c477b354b4ce2bdc91c21678b70b0c478a563252
xzz <you@example.com>
2016-11-15 20:46:42 +0800

    add rulerview

---
+++ demotest/RulerView.swift
@@ -0,0 +1,21 @@
+//
+//  RulerView.swift
+//  demotest
+//
+//  Created by zizhe on 15/11/2016.
+//  Copyright © 2016 zizhe. All rights reserved.
+//
+
+import UIKit
+
+class RulerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
