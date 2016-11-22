408763283fd67f62a762096cfefbc6d87efba65f
xzz <you@example.com>
2016-11-22 18:08:46 +0800

    add basic files

---
+++ demotest/StartButton.swift
@@ -0,0 +1,21 @@
+//
+//  StartButton.swift
+//  demotest
+//
+//  Created by zizhe on 22/11/2016.
+//  Copyright © 2016 zizhe. All rights reserved.
+//
+
+import UIKit
+
+class StartButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ demotest/TimeCounter.swift
@@ -0,0 +1,21 @@
+//
+//  TimeCounter.swift
+//  demotest
+//
+//  Created by zizhe on 22/11/2016.
+//  Copyright © 2016 zizhe. All rights reserved.
+//
+
+import UIKit
+
+class TimeCounter: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
